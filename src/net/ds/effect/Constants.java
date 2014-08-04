0386eaa3301525304348f357c864209f0229a7ed
shixiaolei <shixiaolei@360.cn>
2014-08-04 10:53:41 +0800

    first commit

---
+++ src/net/ds/effect/Constants.java
@@ -0,0 +1,11 @@
+
+package net.ds.effect;
+
+public class Constants {
+    
+    public static final boolean LOGE_ENABLED = true;
+
+    public static final boolean LOGW_ENABLED = true;
+
+    public static final boolean LOGD_ENABLED = true;
+}
